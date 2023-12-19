1af55e3cd7af6602e010fe6b4b28833443ec6fb2
JohnChewning <150081402+JohnChewning@users.noreply.github.com>
2023-12-18 22:26:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ let questions = [
             "call function startGame",
             "Call.startGame",
         ],
-        answer: "myFunction()",
+        answer: "startGame()",
     },
 
     {
